7512e7a278d301f4dae4344e59951bbe2dc94f15
Sylvestre <pasytchangwa@gmail.com>
2021-10-07 20:20:53 +0300

    Fix linters

--- my_enumerable.rb
+++ my_enumerable.rb
@@ -1,19 +1,17 @@
 module MyEnumerable
   def any?
-    each { |e| return true if yield e  }
+    each { |e| return true if yield e }
     false
   end
 
   def all?
-    each {  |e| return false unless yield e  }
+    each { |e| return false unless yield e }
     true
   end
 
   def filter
     new_list = []
-    each {  |e| new_list.push(e) if yield e  }
+    each { |e| new_list.push(e) if yield e }
     new_list
   end
 end
-
-
--- my_list.rb
+++ my_list.rb
@@ -7,16 +7,7 @@ class MyList
     @list = list
   end
 
-  def each
-    @list.each {  |i| yield(i)  }
+  def each(&block)
+    @list.each(&block)
   end
 end
-
-list = MyList.new(1, 2, 3, 4)
-puts list.all? {|e| e < 5}
-puts list.all? {|e| e > 5}
-list.any? {|e| e == 2}
-list.any? {|e| e == 5}
-list.filter {|e| e.even?}
-
-
